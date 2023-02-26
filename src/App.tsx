968afe3ef3321774fbb4506979f93e58d8c11e42
Roma <elusiuve15.09.93@icloud.com>
2023-02-26 19:33:46 +0200

    Created component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {WishList} from "./Components/WishList";
 
 function App() {
     return (
         <div className="text-3xl text-blue-500 flex justify-center">
-            WishList!!!
+            <WishList/>
         </div>
     );
 }
